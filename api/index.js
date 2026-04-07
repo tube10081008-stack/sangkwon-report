ce17845623a6710feae577917b01dc56fae07858
tube10081008-stack <tube10081008@gmail.com>
2026-04-07 15:09:42 +0900

    feat: Vercel serverless에 코라 백엔드 연결

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import apiRoutes from '../server/routes/api.js';
+import coraRoutes from '../server/routes/coraRouter.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,5 +17,6 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api', apiRoutes);
+app.use('/api/cora', coraRoutes);
 
 export default app;
